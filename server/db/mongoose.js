6b2e2bde07392b3ed356cd3051346d0553cac2b0
Tunde <tundeogunjimi@gmail.com>
2018-09-13 14:22:48 +0100

    Update config to use json file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp');
-// mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
